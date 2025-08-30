e3a2c6169d8c87edb9ac4c121f0af6e9489f3c0e
Ahadjon <132246387+ahadjondev@users.noreply.github.com>
2025-08-31 08:50:06 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
-
+ 
 /*========== scroll sections active link ==========*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
